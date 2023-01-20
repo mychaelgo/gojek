bb2157ebae00019e081ebd49730d5659460ccc9a
Mychael Go <admin@mychaelgo.com>
2023-01-20 17:16:40 +0700

    ci: change package name
    Signed-off-by: Mychael Go <admin@mychaelgo.com>

--- goid-generate-node.sh
+++ goid-generate-node.sh
@@ -1,7 +1,9 @@
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/goid-gojekapi.yaml -g typescript-axios --additional-properties=stringEnums=true,modelPropertyNaming=original,npmName=goid-gojek,useSingleRequestParameter=true -o /local/sdk/goid-gojek-node
+# https://docs.github.com/en/packages/quickstart
+
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/goid-gojekapi.yaml -g typescript-axios --additional-properties=npmRepository=https://npm.pkg.github.com,stringEnums=true,modelPropertyNaming=original,npmName=@mychaelgo/goid-gojek,useSingleRequestParameter=true -o /local/sdk/goid-gojek-node
 cp .npmrc.example sdk/goid-gojek-node/.npmrc
 cd sdk/goid-gojek-node
-sedRepoString=", \"publishConfig\": { \"@mychaelgo:registry\": \"https:\/\/npm.pkg.github.com\" }"
+sedRepoString=", \"repository\": { \"type\": \"git\", \"url\": \"git+https:\/\/github.com\/mychaelgo\/gojek.git\" }"
 sed -i -e "$(( $( wc -l < package.json) -1 ))s/$/\n$sedRepoString/" package.json
 rm package.json-e
 npm i
