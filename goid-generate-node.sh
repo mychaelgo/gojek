79066b2618cd808c6ddb3fcd6de4f3171c252dd6
Mychael Go <admin@mychaelgo.com>
2023-01-20 16:55:52 +0700

    ci: change script for generate node sdk
    Signed-off-by: Mychael Go <admin@mychaelgo.com>

--- goid-generate-node.sh
+++ goid-generate-node.sh
@@ -1,3 +1,8 @@
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/goid-gojekapi.yaml -g typescript-axios --additional-properties=stringEnums=true,modelPropertyNaming=original,npmName=goid-gojek,useSingleRequestParameter=true -o /local/sdk/goid-gojek-node
+cp .npmrc.example sdk/goid-gojek-node/.npmrc
 cd sdk/goid-gojek-node
-npm i && npm run build
+sedRepoString=", \"publishConfig\": { \"@mychaelgo:registry\": \"https:\/\/npm.pkg.github.com\" }"
+sed -i -e "$(( $( wc -l < package.json) -1 ))s/$/\n$sedRepoString/" package.json
+rm package.json-e
+npm i
+npm run build
